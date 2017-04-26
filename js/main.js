fdf5ca39df9432b464fad75bc3c8abb5ad838eea
dccruz <dccoder@icloud.com>
2017-04-26 07:38:34 -0700

    Java script example 1

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//when the page is ready, do this 
+
+// premade  
+// $(document).ready();
+
+// lesson 1
+// // print "hello world" to the console
+// console.log("hello world")
+// //print "hello world" to the webpage
+// document.write("hello world")
+// //print "hello world" to pop up dialog box
+// alert("hello world")
+
+var name = prompt("What's Your Name?")
+console.log(name)
